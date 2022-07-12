f91183945395f5467beaaa58489b38e3e104b161
Christopher Ellis <royalpraetorian@gmail.com>
2022-07-12 15:39:47 -0600

    Updated playlist address

--- routes/youtube.js
+++ routes/youtube.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const youtubeService = require('../services/youtubeService');
 
 router.get('/getPlaylistVideos', async function(req, res) {
-    var youtubeVids = (await youtubeService.getVideosFromPlaylist('PLQdvZmDLepiAl4fbNqwFRKVfcdlSl9oq4', 50))
+    var youtubeVids = (await youtubeService.getVideosFromPlaylist('PLVnN9RqB5lrxLXVLlz-JCp_CrstjB3pLN', 50))
     console.log(youtubeVids)
     res.send(youtubeVids.data);
 })
